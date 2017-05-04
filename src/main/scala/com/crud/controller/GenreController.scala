b2d1ac80e7d06e919762743f88d41bfe89f894dc
Sem <babenko.semen@gmail.com>
2017-05-05 00:32:32 +0300

    Save genre action
    Add movie dto
    
    Add movie dto
    
    Add movie dto

--- src/main/scala/com/crud/controller/GenreController.scala
+++ src/main/scala/com/crud/controller/GenreController.scala
@@ -2,6 +2,7 @@ package com.crud.controller
 
 import javax.inject.{Inject, Singleton}
 
+import com.crud.model.Genre
 import com.crud.service.GenreService
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
@@ -15,6 +16,12 @@ class GenreController @Inject()(genreService: GenreService) extends Controller{
   get("/genres") { request: Request =>
     info("genres")
     genreService.getAll()
+  }
+
+  post("/genres") { genre: Genre =>
+    info("genres")
+    genreService.insert(genre)
+    response.created
   }
 
 }
---
+++ src/main/scala/com/crud/model/Movie.scala
@@ -0,0 +1,8 @@
+package com.crud.model
+
+/**
+  * Created by baben on 05.05.2017.
+  */
+case class Movie(name:String, id: Option[Int]){
+
+}

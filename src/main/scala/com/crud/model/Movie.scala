9021143d78c6252591948b8b4191bf3dfcb65840
Babenko <babenkosemen@gmail.com>
2017-05-07 23:14:12 +0300

    Add movie table

--- src/main/scala/com/crud/model/Movie.scala
+++ src/main/scala/com/crud/model/Movie.scala
@@ -3,6 +3,6 @@ package com.crud.model
 /**
   * Created by baben on 05.05.2017.
   */
-case class Movie(name:String, id: Option[Int]){
+case class Movie(name:String, description:String, genreId: Int, id: Option[Int]){
 
 }
---
+++ src/main/scala/com/crud/repository/table/Movies.scala
@@ -0,0 +1,18 @@
+package com.crud.repository.table
+
+import com.crud.model.{Genre, Movie}
+import slick.lifted.Tag
+import slick.jdbc.H2Profile.api._
+
+/**
+  * Created by baben on 07.05.2017.
+  */
+class Movies (tag: Tag) extends Table[Movie] (tag, "Movies") {
+
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def genreId = column[Int]("GENRE_ID")
+  def name = column[String]("NAME")
+  def description = column[String]("DESCRIPTION")
+
+  def * = (name, description, genreId, id.?) <> (Movie.tupled, Movie.unapply)
+}

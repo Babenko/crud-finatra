79f1ee85da273da43cfd8324b7c5325f9efb8051
Babenko <babenkosemen@gmail.com>
2017-05-14 19:05:15 +0300

    Save movie

--- src/main/scala/com/crud/controller/MovieController.scala
+++ src/main/scala/com/crud/controller/MovieController.scala
@@ -2,6 +2,7 @@ package com.crud.controller
 
 import javax.inject.{Inject, Singleton}
 
+import com.crud.model.Movie
 import com.crud.service.MovieService
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
@@ -15,6 +16,12 @@ class MovieController @Inject()(movieService: MovieService) extends Controller {
   get("/movies") { request: Request =>
     info("movies")
     movieService.getAll()
+  }
+
+  post("/movies") { movie: Movie =>
+    info("movies")
+    movieService.insert(movie)
+    response.created
   }
 
 }

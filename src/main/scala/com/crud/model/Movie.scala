72797be11aa30065d3c6c3b5d1fb2148582343b1
Babenko <babenkosemen@gmail.com>
2017-05-18 23:44:26 +0300

    Change field name

--- src/main/scala/com/crud/model/Movie.scala
+++ src/main/scala/com/crud/model/Movie.scala
@@ -1,6 +1,8 @@
 package com.crud.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
   * Created by Sem Babenko on 05.05.2017.
   */
-case class Movie(name:String, description:String, genreId: Int, id: Option[Int] = None)
+case class Movie(name:String, description:String, @JsonProperty("genreId") genreId: Int, id: Option[Int] = None)

c1c12a6b0d38fa3dbfa3040537a058187ebf77ba
Babenko <babenkosemen@gmail.com>
2017-06-01 22:24:45 +0300

    Add route

--- src/main/resources/webapp/app/src/App.js
+++ src/main/resources/webapp/app/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import MovieComponent from './components/movie'
+
 
 class App extends Component {
+
+  state = {
+    movies: null
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:9999/movies")
+    .then(res => res.json())
+    .then(movies => {this.setState({movies}); console.log("fetch", this)})
+  }
+
+
   render() {
+    console.log("this", this)
+    const {movies} = this.state
     return (
+      <Router>
+      <div>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,8 +33,21 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      <div>
+      {
+        movies ? movies.map(movie => (<span> {movie.name} </span>)) : (<span>Loading...</span>)
+      }
+      </div>
+      <Link to={'/1'}>
+      link to one
+      </Link>
+            <Route path="/1" component={MovieComponent}/>
+      </div>
+      </Router>
     );
   }
+
+
 }
 
 export default App;
---
+++ src/main/resources/webapp/app/src/components/movie.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MovieComponent extends Component {
+
+	render(){
+		return <div> Current title is {this.props.name} !!!!!!!!</div>
+	}
+
+}
+
+export default MovieComponent
